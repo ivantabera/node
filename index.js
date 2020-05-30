7bb3b0fc00f04cbe0cc6c18ea1fb02870eab642d
ivanTabera <ivantabera19@gmail.com>
2020-05-29 19:33:31 -0500

    puerto HTTP en node para construir api restful

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-let nombre = "Ivan tabera"
-console.log(`hola ${nombre}`)
+/* UBICAMOS LOS REQUERIMIENTOS */
+const http = require('http');
+
+/* SALIDA DEL PUERTO HTTP */
+http.createServer((req, res) =>{
+
+    /* SALIDA JSON */
+    res.writeHead(200, {'Content-Type':'application/json'})
+    
+    let salida = {
+    nombre: "Ivan",
+        edad: 31,
+        url: req.url
+    }
+
+    res.write(JSON.stringify(salida));
+    res.end();
+
+}).listen(4000);
+
+console.log("habilitado el puerto 4000");
